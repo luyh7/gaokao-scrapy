86cc30632deab2586eddb4f5d0462f28be068701
LUYH\Administrator <470103427@qq.com>
2018-05-14 23:44:15 +0800

    记录当前爬到的页面

--- ironman/majordetail.py
+++ ironman/majordetail.py
@@ -9,6 +9,9 @@ import os
 # cmdline.execute("scrapy crawl majordetail -a index=0".split())
 # LOGGER.setLevel(logging.WARNING)
 for i in range(33, config.MAX_MAJOR_DETAIL_INSECTS):
+    filename = 'MAJOR_DETAIL_RECORD'
+    with open(filename, 'a+') as fout:
+        fout.writelines(['Current page: %d' % i])
     reload(sys)
     sys.setdefaultencoding('utf-8')
     os.system("taskkill /f /im phantomjs.exe")
